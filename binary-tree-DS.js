b6601053fae21b0b183014da87093de02597c973
marklin2289 <marklin2289@gmail.com>
2022-11-03 15:45:12 -0700

    added binary-tree-DS.js

---
+++ binary-tree-DS.js
@@ -0,0 +1,49 @@
+//           root: no parents node
+//   tree =   1
+//         /     \
+//        2       3 : we can storage anything into node: integer, string, object,etc
+//      /   \   /   \ : edges
+//     4     5 6     7
+//   /   \     leaf  leaf: 0 children
+//  8     9
+// leaf   leaf
+
+// a tree can contain many nodes
+// a node can point to other node, we draw a circle for a node
+
+// root that has no parents node
+// leaf node that has no children node on different level
+// parent child relationship
+
+// requirements for binary tree:
+// binary tree = at most 2 children , less than 2 children node including 0 , 1 , 2
+//             - at most 2 children per node
+//             - exactly 1 root
+//             - exactly 1 path b/w root and any node (example: only one path from root 1 to node 7)
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//            a
+//          /    \
+//         b      c
+//       /   \     \
+//      d     e     f
--- nodeDepth.js
+++ nodeDepth.js
@@ -11,150 +11,86 @@
 //     4     5 6     7
 //   /   \
 //  8     9
+//        4
 
-function nodeDepths(root) {
-  let counter = 0;
-  let depth;
-  if (value) {
-    if (!this.left) {
-      counter++;
-      console.log(counter);
-    }
-    if (!this.right) {
-      counter++;
-    }
-    depth = counter;
+// ======================================================
+const nodeDepthsRecursive = (root) => {
+  let result;
+  if (root === null) return [];
+  const leftValues = nodeDepthsRecursive(root.left); // [b,d,e]
+  const rightValues = nodeDepthsRecursive(root.right); // [c,f]
+  result = [root.val, ...leftValues, ...rightValues];
+  console.log(result);
+  return result;
+};
+// ======================================================
+
+const nodeDepths = (root) => {
+  if (root === null) return [];
+  // case of when root is null
+
+  const result = [];
+  const stack = [root]; // first in last out
+  // we are gonna use .push() to add into stack
+  // we are gonna use .pop() to remove from the top stack
+  while (stack.length > 0) {
+    const current = stack.pop(); // instance of Node
+    result.push(current.val);
+
+    if (current.right) stack.push(current.right); //same
+    if (current.left) stack.push(current.left); //if left node exists, push
   }
-}
+  //   console.log(result);
+  return result;
+};
 
 // This is the class of the input binary tree.
-class BinaryTree {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-  //   check(value) {
-  //     const newNode = new Node(value);
-  //     if (this.value) {
-  //       this.value = newNode;
-  //       return this;
-  //     }
-  //   }
-}
-
-/**
-  //  / A binary tree is a tree data structure in which each node has at most two children, 
-  //  which are referred to as the left child and the right child.
-  // // Left child is always less than it's parent and the right child is always bigger than it's parent.
-  
-  class Node {
-      constructor(value) {
-        this.value = value;
-        this.right = null;
-        this.left = null;
-      }
-    }
-    
-    class BinarySearchTree {
-      constructor() {
-        this.root = null;
-      }
-      //inserts a number into the tree. Returns the entire tree.
-      insert(value) {
-        const newNode = new Node(value);
-        if (!this.root) {
-          this.root = newNode;
-          return this;
-        }
-        let current = this.root;
-        const rnLoop = true;
-        while (rnLoop) {
-          if (value === current.value) return undefined;
-          if (value < current.value) {
-            if (!current.left) {
-              current.left = newNode;
-              return this;
-            }
-            current = current.left;
-          } else {
-            if (!current.right) {
-              current.right = newNode;
-              return this;
-            }
-            current = current.right;
-          }
-        }
-      }
-      //finds the given number and returns it. If its not found, returns `null` or `undefined`.
-      find(value) {
-        if (!this.root) return null;
-        let current = this.root;
-        const rnLoop = true;
-        while (rnLoop) {
-          if (!current) return undefined;
-          if (value === current.value) return current;
-          if (value < current.value) {
-            current = current.left;
-          } else {
-            current = current.right;
-          }
-        }
-      }
-      //checks if a given number exists in the tree. If its in the tree, returns `true`, otherwise `false`
-      contains(value) {
-        if (!this.root) return null;
-        let current = this.root;
-        const rnLoop = true;
-        while (rnLoop) {
-          if (!current) return false;
-          if (value === current.value) return true;
-          if (value < current.value) {
-            current = current.left;
-          } else {
-            current = current.right;
-          }
-        }
-      }
-    }
-    
-    
-    
-    
-    //EXAMPLES==================================================================================
-    
-    const binarySearchTree = new BinarySearchTree();
-    binarySearchTree.insert(10); //returns the entire list
-    binarySearchTree.insert(6); //returns the entire list
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(20);
-    binarySearchTree.insert(34);
-    binarySearchTree.insert(69);
-    binarySearchTree.insert(4);
-    binarySearchTree.find(4); //returns `Node {value: 2, right: Node, left: null}`
-    binarySearchTree.find(20);
-    binarySearchTree.find(123); //returns `undefined`
-    binarySearchTree.contains(6); //returns `true`
-    binarySearchTree.contains(123); //returns `false`
-   * 
-   */
-
-function nodeDepths(root) {
-  return sumDepths(root, 0);
-}
-function sumDepths(node, level) {
-  if (!node) return 0;
-
-  return (
-    level + sumDepths(node.left, level + 1) + sumDepths(node.right, level + 1)
-  );
-}
-
-// This is the class of the input binary tree.
-class BinaryTree {
-  constructor(value) {
-    this.value = value;
+class Node {
+  constructor(val) {
+    this.val = val;
     this.left = null;
     this.right = null;
   }
 }
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+//            a
+//          /   \
+//         b     c
+//       /   \    \
+//      d     e    f
+nodeDepths(a);
+nodeDepthsRecursive(a);
+
+module.exports = { nodeDepths };
+
+// function nodeDepths(root) {
+//   return sumDepths(root, 0);
+// }
+// function sumDepths(node, level) {
+//   if (!node) return 0;
+
+//   return (
+//     level + sumDepths(node.left, level + 1) + sumDepths(node.right, level + 1)
+//   );
+// }
+
+// This is the class of the input binary tree.
+// class BinaryTree {
+//   constructor(value) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
