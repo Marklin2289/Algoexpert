deaf3b3fca6e6b1e25612c308b724209b5bddf01
marklin2289 <marklin2289@gmail.com>
2022-11-03 13:55:08 -0700

    nodeDepth.js

--- draft.js
+++ draft.js
@@ -1,3 +1,18 @@
 // console.log(70000000 * 10 ** decimals());
 
-console.log(Math.floor(9 / 2));
+// console.log(Math.floor(9 / 2));
+
+// let array = [1, 2, 3, 4, 5, 6];
+
+function nodeDepths(root) {
+  let stack = [[root, 0]],
+    depths = 0;
+  console.log(stack);
+  while (stack.length) {
+    let [node, depth] = stack.pop();
+    depths += depth;
+    node.left && stack.push([node.left, depth + 1]);
+    node.right && stack.push([node.right, depth + 1]);
+  }
+  return depths;
+}
